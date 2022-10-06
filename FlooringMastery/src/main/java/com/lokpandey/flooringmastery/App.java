593831c20791d5e512a13374461cb950a69fd7d0
Lok Prakash Pandey <lokprakashpandey@gmail.com>
2022-10-06 11:10:08 -0400

    M5-FlooringMastery Project Created

---
+++ FlooringMastery/src/main/java/com/lokpandey/flooringmastery/App.java
@@ -0,0 +1,20 @@
+/*
+ * @author Lok Prakash Pandey
+ * email: lokprakashpandey@gmail.com
+ * date: Oct 6, 2022
+ * purpose: Create App App
+ */
+
+package com.lokpandey.flooringmastery;
+
+/**
+ *
+ * @author lokpandey
+ */
+public class App {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
